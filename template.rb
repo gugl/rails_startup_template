4e16c1cb3c18f9b6e713a4d4c8768441ba5409ac
gugl <guenter.glueck@gmail.com>
2014-06-29 19:10:49 +0200

    added foundation and switched to my personal preferences

--- template.rb
+++ template.rb
@@ -1,25 +1,74 @@
+# Helper methods
+# ==================================================
+
+def say_custom(tag, text); say "\033[1m\033[36m" + tag.to_s.rjust(10) + "\033[0m" + "  #{text}" end
+
+def ask_wizard(question)
+  ask "\033[1m\033[36m" + (@current_recipe || "prompt").rjust(10) + "\033[1m\033[36m" + "  #{question}\033[0m"
+end
+
+def yes_wizard?(question)
+  answer = ask_wizard(question + " \033[33m(y/n)\033[0m")
+  case answer.downcase
+    when "yes", "y"
+      true
+    when "no", "n"
+      false
+    else
+      yes_wizard?(question)
+  end
+end
+
+def multiple_choice(question, choices)
+  say_custom('question', question)
+  values = {}
+  choices.each_with_index do |choice,i|
+    values[(i + 1).to_s] = choice[1]
+    say_custom( (i + 1).to_s + ')', choice[0] )
+  end
+  answer = ask_wizard("Enter your selection:") while !values.keys.include?(answer)
+  values[answer]
+end
+
+
+# Questions
+# ==================================================
+
+@use_devise           = yes_wizard?('Devise?')
+@authorization_system = multiple_choice 'Authorization System?', [['Pundit (default)', :pundit], ['Cancan', :cancan]]
+
 # Gems
 # ==================================================
 
 # Segment.io as an analytics solution (https://github.com/segmentio/analytics-ruby)
 gem "analytics-ruby"
-# For encrypted password
-gem "bcrypt-ruby"
+
+if @use_devise
+  gem 'devise'
+else
+  # For encrypted password
+  gem "bcrypt-ruby"
+end
+
 # Useful SASS mixins (http://bourbon.io/)
 gem "bourbon"
 
-# For authorization (https://github.com/ryanb/cancan)
-gem "cancan"
+gem 'figaro'
+gem 'foundation-rails'
+gem 'rails-i18n', '~> 4.0.0'
+gem 'puma'
 
-case ask("Choose Template Engine:", :limited_to => %w[erb haml slim])
-when "haml"
-  # HAML templating language (http://haml.info)
-  gem "haml-rails"
-when "slim"
-  # A lightweight templating engine (http://slim-lang.com)
-  gem "slim-rails"
-when "erb"
+# Add gem for Authorization System
+case @authorization_system
+  when :cancan then gem 'cancan'
+  when :pundit then gem 'pundit' # Simple, robust and scaleable authorization system (https://github.com/elabs/pundit)
 end
+
+# HAML templating language (http://haml.info)
+gem "haml-rails"
+
+# Foundation
+gem 'foundation'
 
 # Simple form builder (https://github.com/plataformatec/simple_form)
 gem "simple_form", git: "https://github.com/plataformatec/simple_form"
@@ -29,8 +78,22 @@ gem "uuidtools"
 gem_group :development do
   # Rspec for tests (https://github.com/rspec/rspec-rails)
   gem "rspec-rails"
+  gem 'better_errors'
+  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :mri_21, :rbx]
+
   # Guard for automatically launching your specs when files are modified. (https://github.com/guard/guard-rspec)
-  gem "guard-rspec"
+  gem 'guard'
+  gem 'guard-bundler'
+  gem 'guard-cucumber'
+  gem 'guard-rails'
+  gem 'guard-rspec'
+  gem 'html2haml'
+  gem 'rails_layout'
+  gem 'rb-fchange', :require=>false
+  gem 'rb-fsevent', :require=>false
+  gem 'rb-inotify', :require=>false
+  gem 'simplecov'
+  gem "erb2haml"
 end
 
 gem_group :test do
@@ -41,7 +104,12 @@ gem_group :test do
   gem "launchy"
   # FactoryGirl instead of Rails fixtures (https://github.com/thoughtbot/factory_girl)
   gem "factory_girl_rails"
-  gem "database_cleaner"
+
+  gem 'cucumber-rails', :require=>false
+  gem 'poltergeist'
+  gem 'database_cleaner', '1.0.1'
+  gem 'email_spec'
+  gem 'launchy'
 end
 
 gem_group :production do
@@ -49,6 +117,9 @@ gem_group :production do
   gem "rails_12factor"
 end
 
+# Environments
+# ==================================================
+environment 'config.action_mailer.default_url_options = { host: "localhost:3000" }', env: 'development'
 
 # Setting up foreman to deal with environment variables and services
 # https://github.com/ddollar/foreman
@@ -60,24 +131,81 @@ run "echo '.env' >> .gitignore"
 # We need this with foreman to see log output immediately
 run "echo 'STDOUT.sync = true' >> config/environments/development.rb"
 
+# Install gems
+# ==================================================
+run 'bundle install'
 
+generate :controller, 'home index'
+route "root 'home#index'"
 
 # Initialize guard
 # ==================================================
 run "bundle exec guard init rspec"
 
-
-
-# Initialize CanCan
+# Initialize Authorization System
 # ==================================================
-run "rails g cancan:ability"
+case @authorization_system
+  when :cancan
+    run 'rails g cancan:ability'
+  when :pundit
+    run 'rails g pundit:install'
+    inject_into_class 'app/controllers/application_controller.rb', 'ApplicationController', "  include Pundit\n"
+end
 
 
+
+# Figaro
+# ==================================================
+run 'rails g figaro:install'
+
+# Testing
+# ==================================================
+run 'rails g rspec:install'
+run 'rails g cucumber:install'
+run 'rails g email_spec:steps'
+
+# Database
+# ==================================================
+run 'rake db:create'
+run 'rake db:migrate'
+
+# Devise
+# ==================================================
+
+if @use_devise
+  run 'rails generate devise:install'
+  run 'rails generate devise User'
+  run 'rake db:migrate'
+
+  # run "echo 'before_action :authenticate_user!' >> app/controllers/application_controller.rb"
+
+  file 'spec/support/devise.rb', <<-CODE
+    RSpec.configure do |config|
+      config.include Devise::TestHelpers, type: :controller
+    end
+  CODE
+end
+
+# Foundation
+# ==================================================
+# run 'rails g foundation:install --haml --force'
+run "rails g simple_form:install --foundation"
+run 'rails g layout:install foundation5 --force'
+# run "echo '@import \"foundation_and_overrides\";' >>  app/assets/stylesheets/application.css.scss"
+run "echo '@import \"framework_and_overrides\";' >>  app/assets/stylesheets/application.css.scss"
+run "rails g layout:navigation"
+
+if @use_devise
+  # uncomment_lines 'config/initializers/devise.rb', 'config.scoped_views = false'
+
+  # https://github.com/plataformatec/markerb
+  # run 'rails generate devise:views users --markerb'
+  # run 'rails generate devise:views --markerb'
+  run 'rails generate layout:devise foundation5'
+end
 
 # Clean up Assets
 # ==================================================
-# Use SASS extension for application.css
-run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.scss"
 # Remove the require_tree directives from the SASS and JavaScript files.
 # It's better design to import or require things manually.
 run "sed -i '' /require_tree/d app/assets/javascripts/application.js"
@@ -86,34 +214,9 @@ run "sed -i '' /require_tree/d app/assets/stylesheets/application.css.scss"
 run "echo >> app/assets/stylesheets/application.css.scss"
 run "echo '@import \"bourbon\";' >>  app/assets/stylesheets/application.css.scss"
 
-
-
-
-# Bootstrap: install from https://github.com/twbs/bootstrap
-# Note: This is 3.0.0
+# Replace ERB files with HAML
 # ==================================================
-if yes?("Download bootstrap?")
-  run "wget https://github.com/twbs/bootstrap/archive/v3.0.0.zip -O bootstrap.zip -O bootstrap.zip"
-  run "unzip bootstrap.zip -d bootstrap && rm bootstrap.zip"
-  run "cp bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css vendor/assets/stylesheets/"
-  run "cp bootstrap/bootstrap-3.0.0/dist/js/bootstrap.js vendor/assets/javascripts/"
-  run "rm -rf bootstrap"
-  run "echo '@import \"bootstrap\";' >>  app/assets/stylesheets/application.css.scss"
-  run "rails g simple_form:install --bootstrap"
-end
-
-
-# Font-awesome: Install from http://fortawesome.github.io/Font-Awesome/
-# ==================================================
-if yes?("Download font-awesome?")
-  run "wget http://fontawesome.io/assets/font-awesome-4.0.3.zip -O font-awesome.zip"
-  run "unzip font-awesome.zip && rm font-awesome.zip && mv font-awesome-4.0.3 font-awesome"
-  run "cp font-awesome/css/font-awesome.css vendor/assets/stylesheets/"
-  run "cp -r font-awesome/fonts public/fonts"
-  run "rm -rf font-awesome"
-  run "echo '@import \"font-awesome\";' >>  app/assets/stylesheets/application.css.scss"
-end
-
+rake 'haml:replace_erbs'
 
 # Ignore rails doc files, Vim/Emacs swap files, .DS_Store, and more
 # ===================================================
@@ -133,22 +236,21 @@ doc/
 .DS_Store
 EOF"
 
-
 # Git: Initialize
 # ==================================================
 git :init
 git add: "."
 git commit: %Q{ -m 'Initial commit' }
 
-if yes?("Initialize GitHub repository?")
-  git_uri = `git config remote.origin.url`.strip
-  unless git_uri.size == 0
-    say "Repository already exists:"
-    say "#{git_uri}"
-  else
-    username = ask "What is your GitHub username?"
-    run "curl -u #{username} -d '{\"name\":\"#{app_name}\"}' https://api.github.com/user/repos"
-    git remote: %Q{ add origin git@github.com:#{username}/#{app_name}.git }
-    git push: %Q{ origin master }
-  end
-end
+# if yes?("Initialize GitHub repository?")
+#   git_uri = `git config remote.origin.url`.strip
+#   unless git_uri.size == 0
+#     say "Repository already exists:"
+#     say "#{git_uri}"
+#   else
+#     username = ask "What is your GitHub username?"
+#     run "curl -u #{username} -d '{\"name\":\"#{app_name}\"}' https://api.github.com/user/repos"
+#     git remote: %Q{ add origin git@github.com:#{username}/#{app_name}.git }
+#     git push: %Q{ origin master }
+#   end
+# end
